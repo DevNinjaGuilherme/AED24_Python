b027d87bf712d4a70e9478bb9251ae80ae99919e
DevNinjaGuilherme <guilhermedrsilva7@gmail.com>
2024-04-29 14:12:32 -0300

    Segunda atividade Semana 1

---
+++ atividade2.py
@@ -0,0 +1,18 @@
+#Um número é dito perfeito, quando é igual a soma dos seus divisores (exceto com o próprio
+# número). Ler um número, exibir os seus divisores e informar se ele é ou não perfeito.
+
+
+numero = int(input("Número: "))
+soma = 0
+
+for i in range(1, int(numero/2)+1):
+    if numero % i == 0:
+        print(f"{i}, ", end="")
+        soma = soma + i
+
+print(f"\nSoma: {soma}")
+
+if soma == numero:
+    print(f"{numero} é um número perfeito")
+else:
+    print(f"{numero} não é um número perfeito")
